277618660a8089190d62019b348b9e098f541f4a
kiosk <kiosk@idayuan.com>
2016-04-03 00:53:52 +0800

    脚本优化

--- error_log.py
+++ error_log.py
@@ -11,18 +11,16 @@ error_log_path=sys.argv[1]
 def count(error_log_path):
 	li = []
 	f = open(error_log_path,'r')
-	lines = len(f.readlines())
-	f.seek(0,0)
-	for i in range(lines):
-		li.append(line)
+	for i in f:
+		li.append(i)
 		if len(li) <= 6:
-			if line.find('error') != -1:
+			if i.find('error') != -1:
 				for i in range(len(li)):
 					print li[i],
 				print '###############\n'
 		else:
 			del li[0]
-			if line.find('error') != -1:
+			if i.find('error') != -1:
 				for i in range(len(li)):
 					print li[i],
 				print '###############\n'
