b1bb049926c1a675717c592d451b62634d2eed8f
kiosk <kiosk@idayuan.com>
2016-04-07 22:36:36 +0800

    modified if to a class.

--- error_log.py
+++ error_log.py
@@ -5,25 +5,27 @@
 #####################
 
 import sys
-
 error_log_path=sys.argv[1]
+class Error(object):
+	def __init__(self,error_log_path):
+		self.error_log_path = error_log_path
 
-def count(error_log_path):
-	li = []
-	f = open(error_log_path,'r')
-	for i in f:
-		li.append(i)
-		if len(li) <= 6:
-			if i.find('error') != -1:
-				for i in range(len(li)):
-					print li[i],
-				print '###############\n'
-		else:
-			del li[0]
-			if i.find('error') != -1:
-				for i in range(len(li)):
-					print li[i],
-				print '###############\n'
-	f.close()
-
-count(error_log_path)
+	def count(self):
+		li = []
+		f = open(self.error_log_path,'r')
+		for i in f:
+			li.append(i)
+			if len(li) <= 6:
+				if i.find('error') != -1:
+					for i in range(len(li)):
+						print li[i],
+					print '#'*10+ '\n'
+			else:
+				del li[0]
+				if i.find('error') != -1:
+					for i in range(len(li)):
+						print li[i],
+					print '#'*10 + '\n'
+		f.close()
+error = Error(error_log_path)
+error.count()
